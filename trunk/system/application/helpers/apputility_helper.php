f51a4b960368a3bca743b0d4dbd947899b060cc5
somushiv@gmail.com <somushiv@gmail.com@7c9208af-cc6c-c06e-1afe-1390f7978229>
2011-01-26 11:36:25 +0000

    security patch for PWH data

--- trunk/system/application/helpers/apputility_helper.php
+++ trunk/system/application/helpers/apputility_helper.php
@@ -46,5 +46,16 @@ function setValidateVariable($vartocheck){
       );
       return $ofactorArray[$defvalue];
  }
+ function checkwrongpwhid($pwhid,$chapterid){
+   $ci=& get_instance();
+   $ci->load->database(); 
+   $checkQuery="select * from tbl_pat_personal 
+          where patient_id=".$pwhid." and chap_id=".$chapterid;
+  $qryResult=$ci->db->query($checkQuery);
+  $numrows=$qryResult->num_rows();
+  if ($numrows==0){
+   redirect("homepage/notauthorised");
+  }       
+ }
 
 ?>
